6ad930db1d98669dd8edc2dabbde8e65aa39c28f
Ekaterina Petrochenkova <epetrochenkova001@yandex.ru>
2021-05-26 20:40:09 +0300

    Второй вариант

--- main.py
+++ main.py
@@ -3,45 +3,59 @@ import os
 # count_file = 0
 # list = 5
 # print(list)
-list = []
-list_one = []
-list_two = []
-list_three = []
+# list = []
+# list_one = []
+# list_two = []
+# list_three = []
+#
+#
+# with open("1.txt", "r", encoding="utf-8") as f:
+#     lines = f.readlines()
+#     name = "1.txt"
+#     count_line = str(len(lines))
+#     list_one.append(name)
+#     list_one.append(count_line)
+#     list_one.append(lines)
+#
+#     # print(f"{name}\n{count_line}\n{lines}\n")
+#     # print(list_one)
+#
+# with open("2.txt", "r", encoding="utf-8") as f:
+#     lines = f.readlines()
+#     name = "2.txt"
+#     count_line = str(len(lines))
+#     list_two.append(name)
+#     list_two.append(count_line)
+#     list_two.append(lines)
+#     # print(f"{name}\n{count_line}\n{lines}\n")
+#     # print(list_two)
+#
+# with open("3.txt", "r", encoding="utf-8") as f:
+#     lines = f.readlines()
+#     name = "3.txt"
+#     count_line = len(lines)
+#     list_three.append(name)
+#     list_three.append(count_line)
+#     list_three.append(lines)
+#     # print(f"{name}\n{count_line}\n{lines}\n")
+#     # print(list_three)
+#
+# list.append(list_one)
+# list.append(list_two)
+# list.append(list_three)
+# # print(sorted(list, key=len))
+#
 
+import os
+text = []
+texts_name =os.listdir("texts")
+for name in texts_name:
+    with open(f"texts/{name}", encoding="utf-8") as f:
+        lines = f.readlines()
+        count_line = str(len(lines))
+        lines = f"{name}\n{count_line}\n{lines}\n"
+        text.append(lines)
 
-with open("1.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-    name = "1.txt"
-    count_line = str(len(lines))
-    list_one.append(name)
-    list_one.append(count_line)
-    list_one.append(lines)
-
-    # print(f"{name}\n{count_line}\n{lines}\n")
-    # print(list_one)
-
-with open("2.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-    name = "2.txt"
-    count_line = str(len(lines))
-    list_two.append(name)
-    list_two.append(count_line)
-    list_two.append(lines)
-    # print(f"{name}\n{count_line}\n{lines}\n")
-    # print(list_two)
-
-with open("3.txt", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-    name = "3.txt"
-    count_line = len(lines)
-    list_three.append(name)
-    list_three.append(count_line)
-    list_three.append(lines)
-    # print(f"{name}\n{count_line}\n{lines}\n")
-    # print(list_three)
-
-list.append(list_one)
-list.append(list_two)
-list.append(list_three)
-# print(sorted(list, key=len))
+fin_text = 
+print(text)
 
